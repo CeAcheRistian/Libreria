3d97e18e2a887cd0a0eddc9c837fd5c56daccdc0
CeAcheRistian <c.monroy.gtz@gmail.com>
2024-11-07 13:42:09 -0600

    add login html

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
+
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
+
+
 def not_found(error):
     return render_template('errors/404.html'), 404
+
 
 def init_app(config):
     app.config.from_object(config)
